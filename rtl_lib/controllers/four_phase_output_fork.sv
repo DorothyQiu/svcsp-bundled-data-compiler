5648a5bcf26f479be699a068ec0935f0dbebdabf
Dorothy Qiu <yuouqiu@usc.edu>
2026-09-22 23:44:21 -0700

    Retire obsolete async RTL components

--- rtl_lib/controllers/four_phase_output_fork.sv
+++
@@ -1,9 +0,0 @@
-// Stateless four-phase launch fanout for independent BODY output handshakes.
-module four_phase_output_fork #(
-  parameter integer M = 1
-) (
-  input  logic         stage_active,
-  output logic [M-1:0] launch
-);
-  assign launch = {M{stage_active}};
-endmodule
